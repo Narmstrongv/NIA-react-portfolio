783c836f34332a63c573bc93cd9f39db43423fa6
Newton <narmstrongv@gmail.com>
2021-10-20 20:18:09 -0400

    fixing photo links/adding css

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Navigation from './components/Navigation';
 import Project from './components/Project';
-//import './App.css';
+import './App.css';
 
 function App() {
   return (
--- react-portfolio/src/components/Project.js
+++ react-portfolio/src/components/Project.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Diy from "../assets/imgs/DIY.png";
 
 const Project = () => {
     return(
@@ -7,7 +8,7 @@ const Project = () => {
             <h3>
                 DIY Paint-N-Sip
             </h3>
-            <img src="public/assets/imgs/DIY.png" alt="DIY Paint-N-Sip" />
+            <img src={Diy} className="pic-1" alt="DIY Paint-N-Sip" />
             <a href="https://twalker7.github.io/HTeamL-Project/">DIY Paint-N-Sip Deployed App</a>
             <p>
             DIY Paint-N-Sip brings the fun of Paint and Sip events into the comfort of your quarantined home! Users can learn how to make a new cocktail and get artistically inspired while enjoying their drink. The user selects their favorite liquor and gets a cocktail recipe that features their choice. Now, the user can click a button to replace that cocktail if they don’t like the first option. Afterwards, they can click a button to display a work of art from the Metropolitan Museum of Art’s exclusive collection and start painting!
